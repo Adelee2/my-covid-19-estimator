54d636f6e0d06897f81febfc123fe59c4bc7f7e6
Adelee2 <34267494+Adelee2@users.noreply.github.com>
2020-04-09 17:34:23 +0100

    update

--- src/estimator.php
+++ src/estimator.php
@@ -1,77 +1,85 @@
 <?php
-header('Content-Type: application/json');
-
-function covid19ImpactEstimator($datas)
+function covid19ImpactEstimator($data)
 {
 
-    $data = json_encode($datas, JSON_FORCE_OBJECT);
-  
-  if($data->periodType =="days") {
-        $infection_time = (int)($data->timeToElapse / 2);
+//    $data = json_encode($datas, JSON_FORCE_OBJECT);
+    if($data["periodType"] =="days") {
+        $infection_time = (int)($data["timeToElapse"] / 2);
     }
-    elseif ($data->periodType =="weeks"){
-       $infection_time =  (int)(($data->timeToElapse * 7)/2);
+    elseif ($data["periodType"] =="weeks"){
+       $infection_time =  (int)(($data["timeToElapse"] * 7)/2);
     }
-    elseif ($data->periodType =="months"){
-        $infection_time =  (int)(($data->timeToElapse * 30)/2);
+    elseif ($data["periodType"] =="months"){
+        $infection_time =  (int)(($data["timeToElapse"] * 30)/2);
     }
     else{
         $infection_time = 0;
     }
-    $impactcurrentlyInfected= $data->reportedCases * 10;
+    $impactcurrentlyInfected= $data["reportedCases"] * 10;
     $impactinfectionsByRequestedTime= $impactcurrentlyInfected * pow(2, $infection_time);
-    $severeimpactcurrentlyInfected=$data->reportedCases * 50;
+    $severeimpactcurrentlyInfected=$data["reportedCases"] * 50;
     $severeimpactinfectionsByRequestedTime= $severeimpactcurrentlyInfected * pow(2, $infection_time);
 
     $IsevereCasesByRequestedTime= 0.15 * $impactinfectionsByRequestedTime;
     $SsevereCasesByRequestedTime = 0.15 * $severeimpactinfectionsByRequestedTime;
-    $IhospitalBedsByRequestedTime = (0.35 * $data->totalHospitalBeds) - $IsevereCasesByRequestedTime;
-    $ShospitalBedsByRequestedTime = (0.35 * $data->totalHospitalBeds) - $SsevereCasesByRequestedTime;
+    $IhospitalBedsByRequestedTime = (0.35 * $data["totalHospitalBeds"]) - $IsevereCasesByRequestedTime;
+    $ShospitalBedsByRequestedTime = (0.35 * $data["totalHospitalBeds"]) - $SsevereCasesByRequestedTime;
 
     $IcasesForICUByRequestedTime= 0.05 * $impactinfectionsByRequestedTime;
     $ScasesForICUByRequestedTime=0.05* $severeimpactinfectionsByRequestedTime;
     $IcasesForVentilatorsByRequestedTime = 0.02 * $impactinfectionsByRequestedTime;
     $ScasesForVentilatorsByRequestedTime= 0.02 * $severeimpactinfectionsByRequestedTime;
 
-    if($data->periodType =="days") {
-        $IdollarsInFlight = $impactinfectionsByRequestedTime * 0.65 * 1.5 * $$data->timeToElapse;
-        $SdollarsInFlight = $severeimpactinfectionsByRequestedTime * 0.65 * 1.5 * $data->timeToElapse;
+    if($data["periodType"] =="days") {
+        $IdollarsInFlight = $impactinfectionsByRequestedTime * 0.65 * 1.5 * $data["timeToElapse"];
+        $SdollarsInFlight = $severeimpactinfectionsByRequestedTime * 0.65 * 1.5 * $data["timeToElapse"];
     }
-    elseif ($data->periodType =="weeks"){
-        $IdollarsInFlight = $impactinfectionsByRequestedTime * 0.65 * 1.5 * $data->timeToElapse * 7;
-        $SdollarsInFlight = $severeimpactinfectionsByRequestedTime * 0.65 * 1.5 * $data->timeToElapse *7;
+    elseif ($data["periodType"] =="weeks"){
+        $IdollarsInFlight = $impactinfectionsByRequestedTime * 0.65 * 1.5 * $data["timeToElapse"] * 7;
+        $SdollarsInFlight = $severeimpactinfectionsByRequestedTime * 0.65 * 1.5 * $data["timeToElapse"] *7;
     }
-    elseif ($data->periodType =="months"){
-        $IdollarsInFlight = $impactinfectionsByRequestedTime * 0.65 * 1.5 * $data->timeToElapse * 30;
-        $SdollarsInFlight = $severeimpactinfectionsByRequestedTime * 0.65 * 1.5 * $data->timeToElapse * 30;
+    elseif ($data["periodType"] =="months"){
+        $IdollarsInFlight = $impactinfectionsByRequestedTime * 0.65 * 1.5 * $data["timeToElapse"] * 30;
+        $SdollarsInFlight = $severeimpactinfectionsByRequestedTime * 0.65 * 1.5 * $data["timeToElapse"] * 30;
     }
     else{
         $IdollarsInFlight = 0;
         $SdollarsInFlight = 0;
     }
-    $out_data = [
-        "data"=> "",
-        "impact" =>[
-                "currentlyInfected"=> $impactcurrentlyInfected,
-                "infectionsByRequestedTime" =>$impactinfectionsByRequestedTime,
-                "severeCasesByRequestedTime" =>$IsevereCasesByRequestedTime,
-                "hospitalBedsByRequestedTime" => $IhospitalBedsByRequestedTime,
-                "casesForICUByRequestedTime" => $IcasesForICUByRequestedTime,
-                "casesForVentilatorsByRequestedTime" => $IcasesForVentilatorsByRequestedTime,
-                "dollarsInFlight" => $IdollarsInFlight
-            ],
-        "severeImpact"=>[
-                "currentlyInfected"=> $severeimpactcurrentlyInfected,
-                "infectionsByRequestedTime"=> $severeimpactinfectionsByRequestedTime,
-                "severeCasesByRequestedTime" =>$SsevereCasesByRequestedTime,
-                "hospitalBedsByRequestedTime" =>$ShospitalBedsByRequestedTime,
-                "casesForICUByRequestedTime" => $ScasesForICUByRequestedTime,
-                "casesForVentilatorsByRequestedTime" => $ScasesForVentilatorsByRequestedTime,
-                "dollarsInFlight" =>$SdollarsInFlight
-            ]
-        ];
-    $newdata = json_encode($out_data,JSON_FORCE_OBJECT);
-    $newdata->data = $data;
+    $out_data = '{
+        "data": {
+                "region": {
+                    "name": $data["name"],
+                    "avgAge": $data["avgAge"],
+                    "avgDailyIncomeInUSD": $data["avgDailyIncomeInUSD"],
+                    "avgDailyIncomePopulation": $data["avgDailyIncomePopulation"]
+                },
+                "periodType": $data["periodType"],
+                "timeToElapse": $data["timeToElapse"],
+                "reportedCases": $data["reportedCases"],
+                "population": $data["population"],
+                "totalHospitalBeds": $data["totalHospitalBeds"]
+            },
+        "impact": {
+                "currentlyInfected": $impactcurrentlyInfected,
+                "infectionsByRequestedTime": $impactinfectionsByRequestedTime,
+                "severeCasesByRequestedTime": $IsevereCasesByRequestedTime,
+                "hospitalBedsByRequestedTime": $IhospitalBedsByRequestedTime,
+                "casesForICUByRequestedTime": $IcasesForICUByRequestedTime,
+                "casesForVentilatorsByRequestedTime": $IcasesForVentilatorsByRequestedTime,
+                "dollarsInFlight": $IdollarsInFlight
+            },
+        "severeImpact":{
+                "currentlyInfected": $severeimpactcurrentlyInfected,
+                "infectionsByRequestedTime": $severeimpactinfectionsByRequestedTime,
+                "severeCasesByRequestedTime": $SsevereCasesByRequestedTime,
+                "hospitalBedsByRequestedTime": $ShospitalBedsByRequestedTime,
+                "casesForICUByRequestedTime": $ScasesForICUByRequestedTime,
+                "casesForVentilatorsByRequestedTime": $ScasesForVentilatorsByRequestedTime,
+                "dollarsInFlight": $SdollarsInFlight
+            }
+        }';
 
-    return $newdata;
+
+    echo $out_data;
 }
