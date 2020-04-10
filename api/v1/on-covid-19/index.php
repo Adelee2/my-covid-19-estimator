12f6a88af0befbe55398536dd9954f2068b3ff24
Adewale Adeleye <adeleyea5@gmail.com>
2020-04-10 15:33:55 +0100

    Api setup

--- api/v1/on-covid-19/index.php
+++ api/v1/on-covid-19/index.php
@@ -9,6 +9,7 @@ header('Content-Type: application/json');
 //Make sure that this is a POST request.
 if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
     //If it isn't, send back a 405 Method Not Allowed header.
+    echo "hello from here";
     header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
     exit;
 }
