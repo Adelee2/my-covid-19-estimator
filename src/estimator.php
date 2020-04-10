e5d320f987182b94b08ee097a09a6e4e064a2ad6
Adelee2 <34267494+Adelee2@users.noreply.github.com>
2020-04-10 03:23:02 +0100

    final update i hope

--- src/estimator.php
+++ src/estimator.php
@@ -71,6 +71,6 @@ function covid19ImpactEstimator($data)
    $newdata = json_decode($out_data,true);
     $newdata['data'] =$data;
 
-    $newdata = json_encode($newdata, JSON_PRETTY_PRINT);
-    echo $newdata;
+//     $newdata = json_encode($newdata, JSON_PRETTY_PRINT);
+    return $newdata;
 }
