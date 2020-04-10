1955f240b12eafe85e2f076509e0bb9b624c8875
Adewale Adeleye <adeleyea5@gmail.com>
2020-04-10 21:50:28 +0100

    Add Updated

--- api/v1/on-covid-19/json/index.php
+++ api/v1/on-covid-19/json/index.php
@@ -13,6 +13,19 @@ header('Content-Type: application/json');
 //Get the raw POST data from PHP's input stream.
 //This raw data should contain XML.
 $postData = trim(file_get_contents('php://input'));
+$postData= '{
+        "region": {
+            "name": "Africa",
+            "avgAge": 19.7,
+            "avgDailyIncomeInUSD": 5,
+            "avgDailyIncomePopulation": 0.71
+        },
+        "periodType": "days",
+        "timeToElapse": 58,
+        "reportedCases": 674,
+        "population": 66622705,
+        "totalHospitalBeds": 1380614
+}';
 
 $httptime  = $_SERVER['REQUEST_TIME'];
 $httprequest = $_SERVER['REQUEST_METHOD'];
