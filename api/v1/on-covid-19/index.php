95633132beacec63ee0a7e50159ae19acc6b539d
Adewale Adeleye <adeleyea5@gmail.com>
2020-04-10 15:37:53 +0100

    Api setup

--- api/v1/on-covid-19/index.php
+++ api/v1/on-covid-19/index.php
@@ -6,13 +6,6 @@ header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type');
 header('Content-Type: application/json');
 
-//Make sure that this is a POST request.
-if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
-    //If it isn't, send back a 405 Method Not Allowed header.
-    echo "hello from here";
-    header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
-    exit;
-}
 
 //Get the raw POST data from PHP's input stream.
 //This raw data should contain XML.
